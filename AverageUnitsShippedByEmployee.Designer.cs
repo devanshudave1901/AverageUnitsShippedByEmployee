194e6dad754486fa79b4ef0e456e2d5aeaebadb1
Devanshu Dave <devanshudave1901@gmail.com>
2021-02-28 17:28:17 -0600

    Add project files.

---
+++ AverageUnitsShippedByEmployee.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace AverageUnitsShippedByEmployee
+{
+    partial class AverageUnitsShippedByEmployee
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelDay = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxUnitsInput = new System.Windows.Forms.TextBox();
+            this.labelEmployee1 = new System.Windows.Forms.Label();
+            this.textBoxEmployee1PastInputs = new System.Windows.Forms.TextBox();
+            this.labelEmployee1AverageOutput = new System.Windows.Forms.Label();
+            this.labelEmployee2 = new System.Windows.Forms.Label();
+            this.textBoxEmployee2PastInputs = new System.Windows.Forms.TextBox();
+            this.labelEmployee2AverageOutput = new System.Windows.Forms.Label();
+            this.labelEmployee3 = new System.Windows.Forms.Label();
+            this.textBoxEmployee3PastInputs = new System.Windows.Forms.TextBox();
+            this.labelOverallAverageOutput = new System.Windows.Forms.Label();
+            this.buttonToEnter = new System.Windows.Forms.Button();
+            this.buttontoReset = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.labelEmployee3AverageOutput = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // labelDay
+            // 
+            this.labelDay.Location = new System.Drawing.Point(26, 25);
+            this.labelDay.Name = "labelDay";
+            this.labelDay.Size = new System.Drawing.Size(68, 29);
+            this.labelDay.TabIndex = 0;
+            this.labelDay.Text = "Day 1";
+            this.toolTip1.SetToolTip(this.labelDay, "Shipment Days ");
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(26, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Units: ";
+            this.toolTip1.SetToolTip(this.label1, "Units");
+            // 
+            // textBoxUnitsInput
+            // 
+            this.textBoxUnitsInput.Location = new System.Drawing.Point(90, 69);
+            this.textBoxUnitsInput.Name = "textBoxUnitsInput";
+            this.textBoxUnitsInput.Size = new System.Drawing.Size(86, 31);
+            this.textBoxUnitsInput.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.textBoxUnitsInput, "Enter your unit");
+            // 
+            // labelEmployee1
+            // 
+            this.labelEmployee1.Location = new System.Drawing.Point(47, 112);
+            this.labelEmployee1.Name = "labelEmployee1";
+            this.labelEmployee1.Size = new System.Drawing.Size(117, 32);
+            this.labelEmployee1.TabIndex = 3;
+            this.labelEmployee1.Text = "Employee &1";
+            this.labelEmployee1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxEmployee1PastInputs
+            // 
+            this.textBoxEmployee1PastInputs.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBoxEmployee1PastInputs.Location = new System.Drawing.Point(26, 147);
+            this.textBoxEmployee1PastInputs.Multiline = true;
+            this.textBoxEmployee1PastInputs.Name = "textBoxEmployee1PastInputs";
+            this.textBoxEmployee1PastInputs.ReadOnly = true;
+            this.textBoxEmployee1PastInputs.Size = new System.Drawing.Size(199, 178);
+            this.textBoxEmployee1PastInputs.TabIndex = 4;
+            // 
+            // labelEmployee1AverageOutput
+            // 
+            this.labelEmployee1AverageOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelEmployee1AverageOutput.Location = new System.Drawing.Point(26, 337);
+            this.labelEmployee1AverageOutput.Name = "labelEmployee1AverageOutput";
+            this.labelEmployee1AverageOutput.Size = new System.Drawing.Size(199, 38);
+            this.labelEmployee1AverageOutput.TabIndex = 5;
+            this.labelEmployee1AverageOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip1.SetToolTip(this.labelEmployee1AverageOutput, "Average for Employee 1");
+            // 
+            // labelEmployee2
+            // 
+            this.labelEmployee2.Location = new System.Drawing.Point(287, 111);
+            this.labelEmployee2.Name = "labelEmployee2";
+            this.labelEmployee2.Size = new System.Drawing.Size(117, 32);
+            this.labelEmployee2.TabIndex = 6;
+            this.labelEmployee2.Text = "Employee &2";
+            this.labelEmployee2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxEmployee2PastInputs
+            // 
+            this.textBoxEmployee2PastInputs.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBoxEmployee2PastInputs.Location = new System.Drawing.Point(246, 147);
+            this.textBoxEmployee2PastInputs.Multiline = true;
+            this.textBoxEmployee2PastInputs.Name = "textBoxEmployee2PastInputs";
+            this.textBoxEmployee2PastInputs.ReadOnly = true;
+            this.textBoxEmployee2PastInputs.Size = new System.Drawing.Size(199, 178);
+            this.textBoxEmployee2PastInputs.TabIndex = 7;
+            // 
+            // labelEmployee2AverageOutput
+            // 
+            this.labelEmployee2AverageOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelEmployee2AverageOutput.Location = new System.Drawing.Point(246, 337);
+            this.labelEmployee2AverageOutput.Name = "labelEmployee2AverageOutput";
+            this.labelEmployee2AverageOutput.Size = new System.Drawing.Size(199, 38);
+            this.labelEmployee2AverageOutput.TabIndex = 8;
+            this.labelEmployee2AverageOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip1.SetToolTip(this.labelEmployee2AverageOutput, "Average for Employee 2");
+            // 
+            // labelEmployee3
+            // 
+            this.labelEmployee3.Location = new System.Drawing.Point(515, 111);
+            this.labelEmployee3.Name = "labelEmployee3";
+            this.labelEmployee3.Size = new System.Drawing.Size(117, 32);
+            this.labelEmployee3.TabIndex = 9;
+            this.labelEmployee3.Text = "Employee &3";
+            this.labelEmployee3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxEmployee3PastInputs
+            // 
+            this.textBoxEmployee3PastInputs.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBoxEmployee3PastInputs.Location = new System.Drawing.Point(464, 146);
+            this.textBoxEmployee3PastInputs.Multiline = true;
+            this.textBoxEmployee3PastInputs.Name = "textBoxEmployee3PastInputs";
+            this.textBoxEmployee3PastInputs.ReadOnly = true;
+            this.textBoxEmployee3PastInputs.Size = new System.Drawing.Size(199, 178);
+            this.textBoxEmployee3PastInputs.TabIndex = 10;
+            // 
+            // labelOverallAverageOutput
+            // 
+            this.labelOverallAverageOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelOverallAverageOutput.Location = new System.Drawing.Point(26, 395);
+            this.labelOverallAverageOutput.Name = "labelOverallAverageOutput";
+            this.labelOverallAverageOutput.Size = new System.Drawing.Size(635, 38);
+            this.labelOverallAverageOutput.TabIndex = 12;
+            this.labelOverallAverageOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonToEnter
+            // 
+            this.buttonToEnter.Location = new System.Drawing.Point(26, 447);
+            this.buttonToEnter.Name = "buttonToEnter";
+            this.buttonToEnter.Size = new System.Drawing.Size(150, 38);
+            this.buttonToEnter.TabIndex = 13;
+            this.buttonToEnter.Text = "&Enter";
+            this.toolTip1.SetToolTip(this.buttonToEnter, "Press enter for functionalility ");
+            this.buttonToEnter.UseVisualStyleBackColor = true;
+            this.buttonToEnter.Click += new System.EventHandler(this.ButtonToEnterClick);
+            // 
+            // buttontoReset
+            // 
+            this.buttontoReset.Location = new System.Drawing.Point(219, 449);
+            this.buttontoReset.Name = "buttontoReset";
+            this.buttontoReset.Size = new System.Drawing.Size(175, 36);
+            this.buttontoReset.TabIndex = 16;
+            this.buttontoReset.Text = "&Reset";
+            this.toolTip1.SetToolTip(this.buttontoReset, "Please Press Reset to clear the form");
+            this.buttontoReset.UseVisualStyleBackColor = true;
+            this.buttontoReset.Click += new System.EventHandler(this.ButtonToResetClick);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(416, 447);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(174, 38);
+            this.buttonExit.TabIndex = 17;
+            this.buttonExit.Text = "&Exit";
+            this.toolTip1.SetToolTip(this.buttonExit, "Press exit to Quit the application.");
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.ButtonToExitClick);
+            // 
+            // labelEmployee3AverageOutput
+            // 
+            this.labelEmployee3AverageOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelEmployee3AverageOutput.Location = new System.Drawing.Point(462, 337);
+            this.labelEmployee3AverageOutput.Name = "labelEmployee3AverageOutput";
+            this.labelEmployee3AverageOutput.Size = new System.Drawing.Size(199, 38);
+            this.labelEmployee3AverageOutput.TabIndex = 18;
+            this.labelEmployee3AverageOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip1.SetToolTip(this.labelEmployee3AverageOutput, "Average for employee 3");
+            // 
+            // AverageUnitsShippedByEmployee
+            // 
+            this.AcceptButton = this.buttonToEnter;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.CancelButton = this.buttontoReset;
+            this.ClientSize = new System.Drawing.Size(675, 507);
+            this.Controls.Add(this.labelEmployee3AverageOutput);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttontoReset);
+            this.Controls.Add(this.buttonToEnter);
+            this.Controls.Add(this.labelOverallAverageOutput);
+            this.Controls.Add(this.textBoxEmployee3PastInputs);
+            this.Controls.Add(this.labelEmployee3);
+            this.Controls.Add(this.labelEmployee2AverageOutput);
+            this.Controls.Add(this.textBoxEmployee2PastInputs);
+            this.Controls.Add(this.labelEmployee2);
+            this.Controls.Add(this.labelEmployee1AverageOutput);
+            this.Controls.Add(this.textBoxEmployee1PastInputs);
+            this.Controls.Add(this.labelEmployee1);
+            this.Controls.Add(this.textBoxUnitsInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelDay);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AverageUnitsShippedByEmployee";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Average Units Shipped By Employee";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxUnitsInput;
+        private System.Windows.Forms.Label labelEmployee1;
+        private System.Windows.Forms.TextBox textBoxEmployee1PastInputs;
+        private System.Windows.Forms.Label labelEmployee1AverageOutput;
+        private System.Windows.Forms.Label labelEmployee2;
+        private System.Windows.Forms.TextBox textBoxEmployee2PastInputs;
+        private System.Windows.Forms.Label labelEmployee2AverageOutput;
+        private System.Windows.Forms.Label labelEmployee3;
+        private System.Windows.Forms.TextBox textBoxEmployee3PastInputs;
+        private System.Windows.Forms.Label labelOverallAverageOutput;
+        private System.Windows.Forms.Button buttonToEnter;
+        private System.Windows.Forms.Button buttontoReset;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label labelEmployee3AverageOutput;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ AverageUnitsShippedByEmployee.cs
@@ -0,0 +1,258 @@
+﻿// Author - Devanshu Dave
+// Date - 25/02/2021
+// Student ID - 100785733   
+// Description - This application which will calculate the average of three emplyoees and the overall average of three employess.
+
+
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AverageUnitsShippedByEmployee
+{
+    public partial class AverageUnitsShippedByEmployee : Form
+    {
+        // constants Declared for Number of Employees as int data type assigned the 3 value to it
+        const int NumberofEmployees = 3;
+        // constants Declared for Number of Days as int data type assigned the 7(This much days are allowed) value to it
+        const int NumberofDays = 7;
+
+
+        // Declared the SHipedday(the present day) to 1 and assigned the int data type to it
+        int shipedday = 1;
+        // Declared the current employee to 1 and assigned the int data type to it.
+        int currentEmployee = 1;
+
+
+        //   int totalUnitsShiped = 0;
+        // 2D Array been declared for entryarray which has number of employees(1) and number of days in it.
+        int[,] entryArray = new int[NumberofEmployees  , NumberofDays ];
+
+        // array for textBox has been declared with employeeTextBoxes variable.
+        TextBox[] employeeTextBoxes;
+        // array for Label has been declared with employeeAverages variable.
+        Label[]  employeeAverages;
+
+        Label[] fontlabel;
+        // form loaded
+        public AverageUnitsShippedByEmployee()
+        {
+            InitializeComponent();
+            // array emplyeeTextBoxes which will have the inputs from the user in it.
+            employeeTextBoxes = new TextBox[] { textBoxEmployee1PastInputs, textBoxEmployee2PastInputs, textBoxEmployee3PastInputs };
+            // array Emplyoee averages which will have the average of each employe stored in it and is the label type.
+            employeeAverages = new Label[]  { labelEmployee1AverageOutput, labelEmployee2AverageOutput, labelEmployee3AverageOutput };
+
+          //  fontlabel = new Label[] { labelEmployee1, labelEmployee2, labelEmployee3 };
+        }
+
+        /// <summary>
+        /// This button will accept the input from the user to perform the functionality inn which it will available till the user 
+        /// enters all emplyoees. This will Take the user and put it in the entry area where all inputs will be displayed and to calculate all employee 
+        /// average along with overall
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonToEnterClick(object sender, EventArgs e) 
+        {
+            // constant declared for minimum units as int data variable and assigned it to 0.
+            const int MininimunUnits = 0;
+            // Decalared constant for Maximum units and assigned the value of 5000 as a int data type
+            const int MaximumUnits = 5000;
+            // Constant declared for MaximumDays as 7 as data type gives as int.
+            const int MaximumDays = 7;
+            
+            // Functionality part
+
+            //If else used for the validate the user's input.
+            // user is only allowed to enter whole number.
+           // so here we used try parse and with int and it will take from entry array we declared above and will use here as the input
+           // comes from the user. 
+            if (int.TryParse(textBoxUnitsInput.Text, out entryArray[currentEmployee - 1, shipedday - 1]))
+            {
+                // Now if the user enters valid input as a whole number. Now its time for validating the input in the range or not.
+                if (entryArray[currentEmployee - 1 , shipedday -1 ] >= MininimunUnits && 
+                    entryArray[currentEmployee -1, shipedday - 1] <= MaximumUnits)
+                {
+                    // to enter the input in the text boxes 
+
+                    employeeTextBoxes[currentEmployee -1 ].Text += entryArray[currentEmployee -1 , shipedday -1 ] + "\r\n";
+                    //labelEmployee1.Font = new Font(this.Font, FontStyle.Bold);
+                    //labelEmployee1.Font = new Font(this.Font, FontStyle.Regular);
+                    // Using if to know that we have entered for 7 dyas.
+                    if ( shipedday++ >= NumberofDays )
+                    {
+                        // employee total is set to 0 with int data type
+                        double employeeTotal = 0;
+                        // Calculation for the averages for all employee using for loop
+                        for (int day = 0; day  < NumberofDays; day++)
+                        {
+                            employeeTotal += entryArray[currentEmployee -1 , day ];
+
+                           
+                        //    fontlabel[currentEmployee + 1].Font = new Font(this.Font, FontStyle.Regular);
+                        }
+                        // printing.
+                        employeeAverages[currentEmployee - 1].Text = "Average: " + Math.Round((float)employeeTotal /  NumberofDays, 4).ToString("0.00");
+                      //  employeeAverages[currentEmployee - 1].Font = new Font(this.Font, FontStyle.Bold);
+                        // So this shiedday us set to 1 again and which will tell that system has to take the input for next emplyee
+                        shipedday = 1;
+                        // currentempoyee incremeantation
+                        currentEmployee++;
+                    }
+                    // check if we entered all emplyees
+                    if (currentEmployee > NumberofEmployees)
+                    {
+                        // sum is decalred with double data type and set to 0;
+                        double sum = 0;
+                        //double sum1;
+
+                        // overall calculation part.
+                        // using for each loop the overall average has been calculated.
+                        // so for each and we have set the condition as int averaging in the array named entry array.
+                        // which will take the value and look in the  entry array.
+                        foreach (int averaging in entryArray)
+                        {
+                            // this will add and assign the value to the sum
+                            // loop will come again add and will assign to sum.
+                            // full way - sum = sum + averaging.
+                            // the loop will have it till it takes the value from each index.
+                            sum += averaging;
+                        }
+                        // this is to calculate the average.
+                        // which the total will be divided by the entryArray lenght( method) which willbe used to divide that to sum(total).
+                        sum = sum / entryArray.Length;
+
+                        // Printing the result and also rounding to 2 decimal
+                        labelOverallAverageOutput.Text = " Average per Day " + Math.Round(sum,2).ToString("0.00");
+
+
+                        // shipedday set to 1 when the all employees has entered 
+                        shipedday = 1;
+                        // Current employee to 0 when all employes has entered
+                        currentEmployee = 0;
+                        // past inputs for first emplyee box has been set to non displaying by putting enable = false.
+                        textBoxEmployee1PastInputs.Enabled = false;
+                        // past inputs for second emplyee box has been set to non displaying by putting enable = false.
+                        textBoxEmployee2PastInputs.Enabled = false;
+                        // past inputs for third emplyee box has been set to non displaying by putting enable = false.
+                        textBoxEmployee3PastInputs.Enabled = false;
+
+                        // unit input set to non accepting when all employees entry has been done
+                        textBoxUnitsInput.Enabled = false;
+                        // button to enter will be off
+                        buttonToEnter.Enabled = false;
+                        // reset button will be focused
+                        buttontoReset.Focus();
+                        // input box empty
+                        textBoxUnitsInput.Text = "";
+
+
+                    }
+                    // this is to display day no in the top -(day 1, day 2 etc...)
+                    labelDay.Text = "Day" + (shipedday);
+                    
+
+                }
+                // if not in the range then this error will be shown
+                else
+                {
+                    // message box to be set and the error msg too.
+                    MessageBox.Show("Enter a value between " + MininimunUnits + "and" + MaximumUnits + "." + " Invalid Entry!");
+                    // input box is set to selectall
+                    textBoxUnitsInput.SelectAll();
+                    // input box is set to be focused
+                    textBoxUnitsInput.Focus();
+                    // input box is set to clear
+                    textBoxUnitsInput.Clear();
+
+                }
+
+
+            }
+            // if not numeric or whole number than this 
+            else
+            {
+                // message box has been set along with message error
+                MessageBox.Show("Enter a Numeric Number or a Valid Numeric Input", " Invalid Entry");
+                // input box is set to selectall
+                textBoxUnitsInput.SelectAll();
+                // input box is set to be focused
+                textBoxUnitsInput.Focus();
+                // input box is set to be clear
+                textBoxUnitsInput.Clear();
+
+
+            }
+        }
+        /// <summary>
+        /// tHIS is to reset the form whole form will be clear and will be available to use.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonToResetClick(object sender, EventArgs e)
+        {
+            // reset form function been called
+            ResetForm();
+
+        }
+        /// <summary>
+        /// This exits the form when user clicks the exit button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonToExitClick(object sender, EventArgs e)
+        {
+            // close()
+            Close();
+        }
+
+        // reset form function
+        private void ResetForm()
+        {
+            // input box been set to be cleat
+            textBoxUnitsInput.Clear();
+            // all past inputs entered by the user in previous will be set to clear (for 1, 2 , 3)
+            textBoxEmployee1PastInputs.Clear();
+            textBoxEmployee2PastInputs.Clear();
+            textBoxEmployee3PastInputs.Clear();
+
+            // all average output for each will set to empty ( for emplyoee 1,2,3 average output boxes)
+            labelEmployee1AverageOutput.Text = String.Empty;
+            labelEmployee2AverageOutput.Text = String.Empty;
+            labelEmployee3AverageOutput.Text = String.Empty;
+            labelOverallAverageOutput.Text = String.Empty;
+            // shepped day back to 1
+            shipedday = 1;
+           
+
+
+            // unit box which takes input will be back to the state where it will take input.
+            textBoxUnitsInput.Enabled = true;
+            // button to enter to set to be entered enable.
+            buttonToEnter.Enabled = true;
+            // input box is set to be focus when reset button is clicked
+            textBoxUnitsInput.Focus();
+            // this is to set day back to 1.
+            labelDay.Text = "Day" + shipedday;
+            // bold the label 
+            labelEmployee1.Font =new Font(this.Font,FontStyle.Bold);
+            labelEmployee2.Font =new Font(this.Font,FontStyle.Regular);
+            labelEmployee3.Font =new Font(this.Font,FontStyle.Regular);
+
+
+        }
+
+    }
+}
